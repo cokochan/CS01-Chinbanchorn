dbd66279e86b8f277a6a98af88660d838e030cad
cokochan <namo.39461@gmail.com>
2023-07-03 16:29:19 +0700

    finished

---
+++ IF_2.py
@@ -0,0 +1,6 @@
+x=11.5
+
+if (isinstance(x,int)):
+    print('x เป็นเลขจำนวนเต็ม')
+else:
+    print('x เป็นเลขทศนิยม')
---
+++ IF_3.py
@@ -0,0 +1,6 @@
+x=5
+
+if(x%2 == 0):
+    print('Even num')
+else:
+    print('NOT even num')
